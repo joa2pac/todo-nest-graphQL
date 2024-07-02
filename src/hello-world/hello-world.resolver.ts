456c788fa3df842814079a1a4cb98be74d173412
joaquin <joaquinahuel93@gmail.com>
2024-07-02 15:09:45 -0300

    update

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, { description: 'Esto es un query', name: 'Hola' })
   helloWorld(): string {
     return 'Hola Mundo';
+  }
+
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
   }
 }
